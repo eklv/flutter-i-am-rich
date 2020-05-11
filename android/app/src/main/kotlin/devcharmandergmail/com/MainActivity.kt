684462a6e1ed27e56a6146fff0bd81ab9ace91d3
suryareddy3301 <suryareddy3301@gmail.com>
2020-05-11 22:19:19 +0530

    initial commit

---
+++ android/app/src/main/kotlin/devcharmandergmail/com/MainActivity.kt
@@ -0,0 +1,6 @@
+package devcharmandergmail.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text("I am rich")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
